9348bd2ed61878e31b9a0b5d7cd453dadf5730d0
umtrey <dfsiii@gmail.com>
2013-02-06 17:39:11 -0600

    it's an empty site! let's get excited.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = 'c339275929f3d3f0154451bc01e552a7f6313f53c7a00a71f8614ab2ae850e1cf08b1d564ee22deb7a91e7ff3e3bd9b1588f949e3038b616c032b5e6b7be728e'
